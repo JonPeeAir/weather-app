3fe00afa2a81f3490ae070d618df825418077dad
John Pierre Mendoza <johnpierremendoza@gmail.com>
2022-01-22 11:19:55 +0800

    feat: allow user to search for city weather using search bar

--- src/index.js
+++ src/index.js
@@ -8,13 +8,34 @@ function kelvinToFahrenheit(temp) {
     return (temp - 273.15) * (9 / 5) + 32;
 }
 
-async function getWeatherForCity(city) {
+function showLoadingScreen() {
+    const loadingText = document.getElementById("loading");
+    loadingText.style.display = "block";
+
+    const weatherDisplay = document.getElementById("weather-data");
+    weatherDisplay.style.opacity = "0";
+}
+
+function removeLoadingScreen() {
+    const loadingText = document.getElementById("loading");
+    loadingText.style.display = "none";
+
+    const weatherDisplay = document.getElementById("weather-data");
+    weatherDisplay.style.opacity = "1";
+}
+
+async function getWeatherDataForCity(city) {
+    showLoadingScreen();
+
     const apiKey = "e4e929a7a1c99749d377df0b67d0d9c4";
     const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}`);
     const weatherData = await response.json();
-
     console.log(weatherData);
 
+    return weatherData;
+}
+
+function displayWeatherData(weatherData) {
     document.getElementById("city").textContent = weatherData.name;
     document.getElementById("temp").textContent = kelvinToCelsius(weatherData.main.temp).toFixed(2);
     document.getElementById("temp-high").textContent = kelvinToCelsius(weatherData.main.temp_max).toFixed(2);
@@ -22,9 +43,24 @@ async function getWeatherForCity(city) {
     document.getElementById("feels-like").textContent = kelvinToCelsius(weatherData.main.feels_like).toFixed(2);
     document.getElementById("pressure").textContent = weatherData.main.pressure;
     document.getElementById("humidity").textContent = weatherData.main.humidity;
-
-
 }
 
 
-getWeatherForCity("paranaque");
+const form = document.getElementById("search-form");
+form.onsubmit = () => {
+    const cityInput = document.getElementById("city-input");
+
+    const weatherData = getWeatherDataForCity(cityInput.value);
+    weatherData.then(data => {
+        displayWeatherData(data)
+        removeLoadingScreen();
+    });
+
+    return false;
+}
+
+
+getWeatherDataForCity("paranaque").then(data => {
+    displayWeatherData(data)
+    removeLoadingScreen();
+});
